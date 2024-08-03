47171592bc6467f3d699a11b11a19efd64beeeaa
RJihyeon <03x2_18@yonsei.ac.kr>
2024-08-03 15:18:20 +0900

    feat: layout 전체구성

---
+++ src/components/page/MainPage.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Navigation from "../ui/bar/Navigation";
+import StockSearch from "../ui/bar/StockSearch";
+import Wallet from "../ui/Wallet";
+import OrderForm from "../ui/OrderForm";
+import Guide from "../ui/Guide";
+
+function MainPage() {
+  return (
+    <>
+      <Navigation />
+      <StockSearch />
+      <div className="chart">
+        <div>
+          <h1>차트</h1>
+          <p>달력</p>
+        </div>
+        <div>차트 컴포넌트 들어올 자리</div>
+        <img src="" alt="로고" />
+      </div>
+
+      <div>
+        <Wallet WalletName="잔고" />
+        <Wallet WalletName="총 수익률" />
+      </div>
+
+      <div>
+        <OrderForm /> {/* OrderForm 컴포넌트를 추가 */}
+      </div>
+
+      <div>
+        <Guide />
+      </div>
+    </>
+  );
+}
+
+export default MainPage;
---
+++ src/components/ui/Wallet.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Wallet({ WalletName }) {
+  return (
+    <>
+      <div>
+        <h2>{WalletName}</h2>
+        <img src="" alt="logo" />
+        <p>₩100,000,000</p>
+      </div>
+    </>
+  );
+}
+
+export default Wallet;
