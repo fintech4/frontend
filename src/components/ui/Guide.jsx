8c9f7309f0cd432de42a5935c1f2990b2b645a3d
RJihyeon <03x2_18@yonsei.ac.kr>
2024-08-03 15:18:20 +0900

    feat: layout 전체구성

---
+++ src/components/ui/Guide.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Guide() {
+  return (
+    <>
+      <div>
+        <h2>가이드</h2>
+        <img src="" alt="logo" />
+      </div>
+    </>
+  );
+}
+
+export default Guide;
