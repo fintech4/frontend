b07526479d6eb6846b30083d66b5b9985272ba37
RJihyeon <03x2_18@yonsei.ac.kr>
2024-08-03 13:56:55 +0900

    feat: nav, stock bar 레이아웃 구성

--- stock-simulator/src/App.jsx
+++ stock-simulator/src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./assets/css/styles.css";
+import MainPage from "./components/page/MainPage";
+import MyPage from "./components/page/MyPage";
+import Navigation from "./components/ui/bar/Navigation";
 
-function App(){
+function App() {
   return (
-    <></>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </Router>
   );
 }
 
