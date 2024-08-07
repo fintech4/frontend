baa91a3afdc180674d0af350bcad3c79ce29d1e8
RJihyeon <03x2_18@yonsei.ac.kr>
2024-08-07 11:54:47 +0900

    feat: mypage tooltip 구현 완료

--- src/components/ui/tables/MyTable.jsx
+++ src/components/ui/tables/MyTable.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import { FaInfoCircle } from "react-icons/fa";
+import "react-tooltip/dist/react-tooltip.css"; // react-tooltip의 기본 스타일을 가져옵니다.
+import "../../../assets/css/styles.css";
 
 const TableWrapper = styled.div`
   table {
@@ -34,7 +38,25 @@ const TableWrapper = styled.div`
   .align-right {
     text-align: right;
   }
+  .info-icon {
+    margin-left: 8px;
+    color: #888;
+    cursor: pointer;
+  }
 `;
+
+const tooltipStyles = {
+  boxShadow: "0px 4px 10px 1px rgba(113, 205, 199, 0.3)",
+  borderRadius: "8px",
+  padding: "10px",
+  backgroundColor: "#fff",
+  color: "#15181E", // 수정된 색상
+  fontFamily: "Pretendard Variable",
+  fontSize: "16px", // 수정된 폰트 크기
+  fontWeight: "500",
+  lineHeight: "24px", // 수정된 줄 간격
+  zIndex: "1000",
+};
 
 const Table = () => {
   const [data, setData] = useState({
@@ -64,15 +86,62 @@ const Table = () => {
       <table>
         <tbody>
           <tr>
-            <td className="align-left">총 자산</td>
+            <td className="align-left">
+              총 자산
+              <FaInfoCircle
+                className="info-icon"
+                data-tooltip-id="totalAssetsTip"
+                color="#058077"
+              />
+              <ReactTooltip
+                id="totalAssetsTip"
+                place="bottom"
+                effect="solid"
+                style={tooltipStyles}
+              >
+                보유주식의 총액 + 예수금으로, 현재까지의 총 자산 보유액입니다.
+              </ReactTooltip>
+            </td>
             <td className="align-right">{data.totalAssets.toLocaleString()}</td>
           </tr>
           <tr>
-            <td className="align-left">예수금</td>
+            <td className="align-left">
+              예수금
+              <FaInfoCircle
+                className="info-icon"
+                data-tooltip-id="depositTip"
+                color="#058077"
+              />
+              <ReactTooltip
+                id="depositTip"
+                place="bottom"
+                effect="solid"
+                style={tooltipStyles}
+              >
+                증권 계좌에 있는 현금으로, 내가 주식 매입을 위해 사용할 수 있는
+                <br />
+                금액입니다. 나의 통장 잔고와 같은 존재이죠.
+              </ReactTooltip>
+            </td>
             <td className="align-right">{data.deposit.toLocaleString()}</td>
           </tr>
           <tr>
-            <td className="align-left">보유주식 총액</td>
+            <td className="align-left">
+              보유주식 총액
+              <FaInfoCircle
+                className="info-icon"
+                data-tooltip-id="totalStocksTip"
+                color="#058077"
+              />
+              <ReactTooltip
+                id="totalStocksTip"
+                place="bottom"
+                effect="solid"
+                style={tooltipStyles}
+              >
+                평가금액의 합으로 지금까지 주식투자로만 벌어들인 수익입니다.
+              </ReactTooltip>
+            </td>
             <td className="align-right">{data.totalStocks.toLocaleString()}</td>
           </tr>
           <tr>
