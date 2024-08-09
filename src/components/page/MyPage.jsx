299e4efd1382abde099e7c03c63f803dc6da67d1
RJihyeon <03x2_18@yonsei.ac.kr>
2024-08-09 20:08:43 +0900

    feat: API호출 실패 방어코드 추가(MyPage)

--- src/components/page/MyPage.jsx
+++ src/components/page/MyPage.jsx
@@ -86,6 +86,7 @@ const MyPage = () => {
         setMyAsset(response.data);
       } catch (error) {
         console.error("Error fetching my asset data:", error);
+        setMyAsset(defaultMyAsset);
       }
     };
 
@@ -97,6 +98,7 @@ const MyPage = () => {
         setMyStockList(response.data);
       } catch (error) {
         console.error("Error fetching my stock list data:", error);
+        setMyStockList(defaultMyStockList);
       }
     };
 
